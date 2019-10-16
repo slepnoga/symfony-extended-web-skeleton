bb978099da5830c4cdd85b1098fcdff4efa0e64e
Andrey <andrey@makonix.com>
2019-10-16 23:23:16 +0300

    More User Feature

---
+++ src/Migrations/Version20191016200510.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright (c) 2019.  Slepnoga.
+ */
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191016200510 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql(
+            'ALTER TABLE user ADD last_login DATETIME DEFAULT \'1800-01-01 00:00:00 \', ADD enabled TINYINT(1) NOT NULL, ADD register_data DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE user DROP last_login, DROP enabled, DROP register_data');
+    }
+}
---
+++ src/Migrations/Version20191016200909.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright (c) 2019.  Slepnoga.
+ */
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191016200909 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql(
+            'ALTER TABLE user CHANGE last_login last_login DATETIME DEFAULT \'1800-01-01 00:00:00 \', CHANGE enabled enabled TINYINT(1) DEFAULT \'false\' NOT NULL'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql(
+            'ALTER TABLE user CHANGE last_login last_login DATETIME DEFAULT \'1800-01-01 00:00:00\', CHANGE enabled enabled TINYINT(1) NOT NULL'
+        );
+    }
+}
