5fdb62558f0aaeafe44907772ddee6679e1b513f
Andrey <andrey@makonix.com>
2019-10-19 22:06:40 +0300

    rehsh old password ( sample, wait 4.4)

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+// need 4.4 - wait
+// use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -12,12 +14,24 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository // implements PasswordUpgraderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
+
+    /*
+     * https://symfony.com/blog/new-in-symfony-4-4-password-migrations
+    public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
+    {
+        // this code is only an example; the exact code will depend on
+        // your own application needs
+        $user->setPassword($newEncodedPassword);
+        $this->getEntityManager()->flush($user);
+    }
+    */
+
 
     // /**
     //  * @return User[] Returns an array of User objects
